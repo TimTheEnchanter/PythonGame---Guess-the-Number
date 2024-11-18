56f8d2d2fa8caf943b3896c64980e240d622af50
James Parrott <japarrott@shaw.ca>
2024-11-18 12:48:28 -0700

    add logic

--- main.py
+++ main.py
@@ -19,5 +19,11 @@ def computer_guess(x):
     while feedback != "c":
         guess = random.randint(low, high)
         feedback = input(f"Is guess too high(H), too low (L) or correct (C)?").lower()
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1    
+
+    print(f"The computer guessed your number, {guess}, correctly")
 
 guess(10)
